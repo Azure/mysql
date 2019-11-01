0871478cfe41ac3bfd66541c3d8b2c313ec63f74
Ashish Ranjan <asranja@microsoft.com>
2019-11-01 14:46:53 +0530

    Azure MySQL Action (#1)
    * Azure MySQL Action
    
    * Addressed review comments
    
    * code fixes

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+import * as core from '@actions/core';
+import * as crypto from 'crypto';
+import * as path from 'path';
+import { AuthorizerFactory } from 'azure-actions-webclient/AuthorizerFactory';
+
+import AzureMySqlActionHelper from './AzureMySqlActionHelper';
+import AzureMySqlAction, { IActionInputs } from "./AzureMySqlAction";
+import FirewallManager from './FirewallManager';
+import AzureMySqlResourceManager from './AzureMySqlResourceManager';
+import MySqlConnectionStringBuilder from './MySqlConnectionStringBuilder';
+
+let userAgentPrefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
+
+export async function run() {
+    let firewallManager;
+    try {
+        // Set user agent variable
+        let usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex');
+        let actionName = 'AzureMySqlAction';
+        let userAgentString = (!!userAgentPrefix ? `${userAgentPrefix}+` : '') + `GITHUBACTIONS_${actionName}_${usrAgentRepo}`;
+        core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
+
+        let inputs = getInputs();
+        let azureMySqlAction = new AzureMySqlAction(inputs);
+        let azureResourceAuthorizer = await AuthorizerFactory.getAuthorizer();
+        let azureMySqlResourceManager = await AzureMySqlResourceManager.getResourceManager(inputs.serverName, azureResourceAuthorizer);
+        firewallManager = new FirewallManager(azureMySqlResourceManager);
+
+        await firewallManager.addFirewallRule(inputs.serverName, inputs.connectionString);
+        await azureMySqlAction.execute();
+    }
+    catch(error) {
+        core.setFailed(error.message);
+    }
+    finally {
+        if (firewallManager) {
+            await firewallManager.removeFirewallRule();
+        }
+
+        // Reset AZURE_HTTP_USER_AGENT
+        core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentPrefix);
+    }
+}
+
+function getInputs(): IActionInputs {
+    let serverName = core.getInput('server-name', { required: true });
+    
+    let connectionString = core.getInput('connection-string', { required: true });
+    let connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+    
+    let sqlFile = AzureMySqlActionHelper.resolveFilePath(core.getInput('sql-file', { required: true }));
+    if (path.extname(sqlFile).toLowerCase() !== '.sql') {
+        throw new Error(`Invalid sql file path provided as input ${sqlFile}`);
+    }
+
+    let additionalArguments = core.getInput('arguments');
+    
+    return {
+        serverName: serverName,
+        connectionString: connectionStringBuilder,
+        sqlFile: sqlFile,
+        additionalArguments: additionalArguments   
+    };
+}
+
+run();
