6b9d2de8042adb8cfb8b6cbf7a80b2ba218ee961
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-09-22 09:29:18 +0530

    Merging azure login mandate fix  (#17)
    * Update README for MySQL action (#7)
    
    * Update README for MySQL action
    
    * update
    
    * Validate server name (#8)
    
    * Update README.md
    
    * Bump acorn from 5.7.3 to 5.7.4
    
    Bumps [acorn](https://github.com/acornjs/acorn) from 5.7.3 to 5.7.4.
    - [Release notes](https://github.com/acornjs/acorn/releases)
    - [Commits](https://github.com/acornjs/acorn/compare/5.7.3...5.7.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add L0 tests for MySql action (#11)
    
    * Add workflow file for L0 tests (#12)
    
    * Bump lodash from 4.17.15 to 4.17.19
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update README.md
    
    * Update README.md
    
    * Bump handlebars from 4.4.5 to 4.7.6
    
    Bumps [handlebars](https://github.com/wycats/handlebars.js) from 4.4.5 to 4.7.6.
    - [Release notes](https://github.com/wycats/handlebars.js/releases)
    - [Changelog](https://github.com/handlebars-lang/handlebars.js/blob/master/release-notes.md)
    - [Commits](https://github.com/wycats/handlebars.js/compare/v4.4.5...v4.7.6)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix for adding azure login when firewall is configured (#14)
    
    * fix for azure login context
    * added unit tests
    
    * removing test files
    
    * adding node_modules
    
    * removing node_modules
    
    * updating node_modules
    
    * updating node_modules
    
    Co-authored-by: Ashish Ranjan <asranja@microsoft.com>
    Co-authored-by: UshaN <ushan@microsoft.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Ashish Ranjan <asranja@github.com>

--- src/FirewallManager.ts
+++ src/FirewallManager.ts
@@ -1,27 +1,19 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import AzureMySqlActionHelper from "./AzureMySqlActionHelper";
 import AzureMySqlResourceManager from './AzureMySqlResourceManager';
-
-const ipv4MatchPattern = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 
 export default class FirewallManager {
     constructor(azureMySqlResourceManager: AzureMySqlResourceManager) {
         this._resourceManager = azureMySqlResourceManager;
     }
 
-    public async addFirewallRule(serverName: string, connectionString: any): Promise<void> {
-        let ipAddress = await this._detectIPAddress(serverName, connectionString);
+    public async addFirewallRule(ipAddress: string): Promise<void> {
         if (!ipAddress) {
             core.debug(`Client has access to MySql server. Skip adding firewall exception.`);
             return;
         }
-        
         console.log(`Client does not have access to MySql server. Adding firewall exception for client's IP address.`)
-        
         this._firewallRule = await this._resourceManager.addFirewallRule(ipAddress, ipAddress);
         core.debug(JSON.stringify(this._firewallRule));
-
         console.log(`Successfully added firewall rule ${this._firewallRule.name}.`);
     }
 
@@ -34,38 +26,6 @@ export default class FirewallManager {
         else {
             core.debug('No firewall exception was added.')
         }
-    }
-
-    private async _detectIPAddress(serverName: string, connectionString: any): Promise<string> {
-        let mySqlClientPath = await AzureMySqlActionHelper.getMySqlClientPath();
-
-        let ipAddress = '';
-        let mySqlError = '';
-        
-        try {
-            core.debug(`Validating if client has access to MySql Server '${serverName}'.`);
-            core.debug(`"${mySqlClientPath}" -h ${serverName} -u "${connectionString.userId}" -e "show databases"`);
-            await exec.exec(`"${mySqlClientPath}" -h ${serverName} -u "${connectionString.userId}" -e "show databases"`, [`--password=${connectionString.password}`], {
-                silent: true,
-                listeners: {
-                    stderr: (data: Buffer) => mySqlError += data.toString()
-                }
-            });
-        }
-        catch (error) {
-            core.debug(mySqlError);
-            
-            let ipAddresses = mySqlError.match(ipv4MatchPattern);
-            if (!!ipAddresses) {
-                ipAddress = ipAddresses[0];      
-            }
-            else {
-                throw new Error(`Failed to add firewall rule. Unable to detect client IP Address. ${mySqlError} ${error}`)
-            }
-        }
-
-        //ipAddress will be an empty string if client has access to SQL server
-        return ipAddress;
     }
 
     private _firewallRule: any; // assign proper type
